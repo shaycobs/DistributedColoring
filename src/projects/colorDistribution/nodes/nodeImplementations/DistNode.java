399a4f7d8d379553c0223f944a46f3df3007900e
Lior Chen <liorchen@gmail.com>
2015-06-13 17:51:48 +0300

    Optimized imports

--- src/projects/colorDistribution/nodes/nodeImplementations/DistNode.java
+++ src/projects/colorDistribution/nodes/nodeImplementations/DistNode.java
@@ -1,11 +1,6 @@
 package projects.colorDistribution.nodes.nodeImplementations;
 
 import common.Nodes.BaseDistNode;
-import projects.colorDistribution.nodes.edges.DistBidirectionalEdge;
-import sinalgo.nodes.edges.Edge;
-import sinalgo.nodes.messages.Inbox;
-
-import java.awt.*;
 
 public class DistNode extends BaseDistNode {
 
--- src/projects/fasterColoring/nodes/nodeImplementations/DistNode.java
+++ src/projects/fasterColoring/nodes/nodeImplementations/DistNode.java
@@ -1,11 +1,6 @@
 package projects.fasterColoring.nodes.nodeImplementations;
 
 import common.Nodes.BaseDistNode;
-import projects.colorDistribution.nodes.edges.DistBidirectionalEdge;
-import sinalgo.nodes.edges.Edge;
-import sinalgo.nodes.messages.Inbox;
-
-import java.awt.*;
 
 public class DistNode extends BaseDistNode {
 	
