8942fcd0a3a44fe067d86b05f986f99e12b20404
Lior Chen <liorchen@gmail.com>
2015-07-08 21:51:36 +0300

    Fixed cole-vishkin algo and implemented it on BaseDistNode + added max-degree global counter

---
+++ src/projects/colorDistribution/models/connectivityModels/MaxDegree.java
@@ -0,0 +1,15 @@
+package projects.colorDistribution.models.connectivityModels;
+
+import common.models.connectivityModels.BaseMaxDegree;
+import sinalgo.configuration.CorruptConfigurationEntryException;
+
+
+public class MaxDegree extends BaseMaxDegree{
+    /**
+     * The default constructor for this class.
+     *
+     * @throws CorruptConfigurationEntryException If one of the initialization steps fails.
+     */
+    public MaxDegree() throws CorruptConfigurationEntryException {
+    }
+}
--- src/projects/colorDistribution/nodes/nodeImplementations/DistNode.java
+++ src/projects/colorDistribution/nodes/nodeImplementations/DistNode.java
@@ -1,7 +1,13 @@
 package projects.colorDistribution.nodes.nodeImplementations;
 
 import common.Nodes.BaseDistNode;
+import sinalgo.nodes.messages.Inbox;
 
 public class DistNode extends BaseDistNode {
+
+    @Override
+    public void handleMessages(Inbox inbox) {
+        super.handleMessages(inbox);
+    }
 
 }
